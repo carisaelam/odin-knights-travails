dbe3748ea1d3e983f1f8a78c502c6316c509041e
carisaelam <carisaelam@gmail.com>
2024-06-27 12:12:29 -0400

    Create adjacency_list method
    Method takes in a position, runs possible_moves
    on it, and returns an adjacency list of all
    possible moves of possible moves. In other words,
    it shows you the possibility of all of the
    next moves' next moves.

--- lib/main.rb
+++ lib/main.rb
@@ -10,7 +10,7 @@ def possible_moves(x, y)
   moves.push([x - 2, y + 1])
   moves.push([x - 2, y - 1])
 
-  moves
+  filter_possible_moves(moves)
 end
 
 # filter out illegal moves
@@ -26,5 +26,18 @@ def filter_possible_moves(moves)
   legal_moves
 end
 
-moves = possible_moves(7, 7)
-p filter_possible_moves(moves)
+# return adjacency list for position
+def adjacency_list(possible_moves)
+  adjacency_list = []
+  possible_moves.each_with_index do |move, i|
+    x = move[0]
+    y = move[1]
+    adjacency_list.push(possible_moves(x, y))
+    # puts "move: #{move} / index: #{i}"
+    # puts "adj_list for move #{i}:
+    # #{possible_moves(x, y)}\n\n"
+  end
+  adjacency_list
+end
+
+p adjacency_list(possible_moves(0, 0))
